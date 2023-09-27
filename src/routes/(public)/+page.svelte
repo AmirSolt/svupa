75982f1ad90a65a9af1596c02021b9d9041aecb4
AmirSolt <soltamir194@gmail.com>
2023-09-27 18:24:51 -0400

    uipd

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -6,15 +6,14 @@
     <div class="flex flex-col justify-center items-center text-center gap-8 pt-16">
         <div class="flex flex-col justify-center items-center text-center gap-2">
             <h1 class="text-6xl">
-                Svelte + Supabase
-                =
+                SaaS
                 <strong class="text-6xl md:text-6xl  font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 from-10% via-purple-500 via-30% to-pink-500 to-90%">
-                    Svupa
+                    Template
                 </strong> 
             </h1>
             <div>
                 <p class="text-2xl">
-                    [Text]
+                    Stack: Sveltekit + Supabase
                 </p>
             </div>
             <small class="text-slate-300">
